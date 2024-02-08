8e9dd7d4d0f395c636f95394ea3afe8665f88984
davisiebra <davialsiebra@gmail.com>
2024-02-08 14:47:15 -0300

    refactor: Anonymous login fixed

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -8,11 +8,21 @@ export const AuthProvider = ({children}) => {
     const [data, setData] = useState(null);
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (authUser) => {
-            const docRefResponse = doc(db, "users", authUser.uid);
-            setData({
-                user: authUser,
-                docRef: docRefResponse
-            });
+            if (authUser && !authUser.isAnonymous) {
+                const docRefResponse = doc(db, "users", authUser.uid);
+                setData({
+                    user: authUser,
+                    docRef: docRefResponse
+                });
+            } else {
+                const docRefResponse = doc(db, "visitors", authUser.uid);
+                setData({
+                    user: authUser,
+                    docRef: docRefResponse
+                });
+            }
+
+            
         });
     
         return () => unsubscribe();
